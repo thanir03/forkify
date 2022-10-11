6d1eafac63e4c277a9bfd7b31fccce65034f3b74
thanir03 <2003thanir10@gmail.com>
2022-10-11 14:55:04 +0800

    Edit gitignore

---
+++ src/js/config.js
@@ -0,0 +1,9 @@
+// additional information
+export const API_KEY = `22db2e81-e6f8-4357-871f-550834935030`;
+export const TIME_OUT_DELAY = 5;
+export const API_URL = `https://forkify-api.herokuapp.com/api/v2/recipes/`;
+export const RESULT_PER_PAGE = 15;
+export const DELAY_TIME_RENDER = 2.5
+const API_DOCUMENTATION = `https://forkify-api.herokuapp.com/v2`;
+
+// STORE IMPORTANT INFORMATION
